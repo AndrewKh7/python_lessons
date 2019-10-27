60f4c52878a597ad8bffba7fd0b52e39fb9dbb13
Andrey Khrapkov <andrey.i.khrapkov@gmail.com>
2019-10-27 23:17:38 +0300

    Finished second week

--- python_les2_2/les24_dop.py
+++ python_les2_2/les24_dop.py
@@ -1,4 +1,13 @@
 import os
+output_path = []
+for p,d,f in os.walk('./main'):
+    for i in f:
+        if i[-3:] == '.py':
+            output_path.append(p[2:])
+            break
 
-os.chdir('main')
-
+output_path.sort()
+for i in output_path:
+        print(i)
+with open('../out.txt','w') as file_:
+    file_.write('\n'.join(output_path))
